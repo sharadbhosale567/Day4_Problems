7f587cd0f4fae520279714fd1656ed6bb5dd534b
sharad <sharadbhosale567@gmail.com>
2022-03-10 14:20:05 +0530

    [Sharad] my first commit

---
+++ Addition.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 25;
+        int c = a + b;
+        System.out.println(c);
+    }
+}

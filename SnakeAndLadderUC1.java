915a0a299d4ee5111b27573f792eadbd91d5e074
sharad <sharadbhosale567@gmail.com>
2022-03-14 12:58:47 +0530

    [Sharad] Snake

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class SnakeAndLadderUC1 {
+    public static void main(String[] args) {
+        int player;
+        int snake;
+        int ladder;
+        int Start =0;
+        if( Start == 0 ){
+            System.out.println("Start");
+
+        }
+    }
+}
---
+++ SnakeAndLadderUC2.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import java.util.Random;
+
+public class SnakeAndLadderUC2 {
+    public static void main(String[] args) {
+
+        double Start = Math.floor(Math.random()) % 6 + 1;
+        int player;
+        int snake;
+        int ladder;
+
+        if( Start == 0 ){
+            System.out.println("Start");
+        }
+        else {
+            System.out.println("no Start");
+        }
+
+    }
+}
